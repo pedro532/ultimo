b5b55a4f85c06d91ca84875ded1e114bde1c7e21
pedro <mahcarponel12@gmail.com>
2022-02-01 20:28:31 +0100

    a la v

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Welcome to LAMP Infrastructure</title>
+        <title>Welcome to LAMP a la ve</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
