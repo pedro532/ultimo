0800d02a2d49320989e52b3c67314623dcf12ee5
pedro <mahcarponel12@gmail.com>
2022-02-01 20:44:40 +0100

    segundo

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Welcome to LAMP a la ve</title>
+        <title>Welcome t LAMP a la verg</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
